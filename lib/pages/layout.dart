71e77f7c6978dca7598a47538d0e601b1ca9a402
nimr77 <nimr.h.sawafta@gmail.com>
2025-07-08 01:32:28 +0100

    update

--- lib/pages/layout.dart
+++ lib/pages/layout.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class LayoutPage extends StatelessWidget {
   final Widget child;
@@ -6,6 +6,6 @@ class LayoutPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(body: child);
   }
 }
